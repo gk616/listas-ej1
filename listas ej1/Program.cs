c98ec24cc34dabc9181cccf7689c1517ce4e4fe5
gk616 <ilovepenises67@gmail.com>
2024-04-29 21:23:37 +0200

    commit 1

---
+++ listas ej1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("Ingresa una serie de números enteros separados por espacios:");
+string entrada = Console.ReadLine();
+
+string[] serie = entrada.Split(' ');
+
+List<int> numeros = new List<int>();
+
+int suma = 0;
+int valoresNoValidos = 0;
+
+foreach (string numeroTexto in serie)
+{
+    try
+    {
+        int numero = int.Parse(numeroTexto);
+        numeros.Add(numero);
+        
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"Error: '{numeroTexto}' no es un número válido.");
+        valoresNoValidos++;
+    }
+}
+
+
+foreach (int numero in numeros)
+{
+    suma += numero;
+}
+Console.WriteLine($"La suma total de los números ingresados es: {suma}");
